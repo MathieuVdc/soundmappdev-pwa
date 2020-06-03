5d7c05ae3b9b08e5ea042497f4abb83e6fe50e81
mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>
2020-06-03 16:18:14 +0200

    first

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  serverUrl: 'https://admindev.soundmapp.fr/api',
+  appUrl: 'https://godev.soundmapp.fr',
+  appImageUrl: 'https://trynearme.app/assets/img/nearme.png',
+  appId: 'PrftxcVqjt',
+  fbId: '806556149777229',
+  googleMapsApiKey: 'AIzaSyDlCzOqSfL9wSH2py7AqmL6KTpytnkARoE',
+  androidHeaderColor: '#d5007d',
+  defaultUnit: 'km',
+  defaultLang: 'en',
+  radiusSearch: 0,
+  googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
+  oneSignal: {
+    appId: 'ec3ab19c-b595-437b-ae41-d74c2a553cf9',
+    googleProjectNumber: '168511274183'
+  },
+};
+
